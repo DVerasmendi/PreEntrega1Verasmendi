0abf9b27ade77e7918e7e2196a40fb5994367183
DVerasmendi <54422022+DVerasmendi@users.noreply.github.com>
2023-09-04 20:02:31 -0300

    ++

--- js/style.js
+++ js/style.js
@@ -95,7 +95,7 @@ plans.forEach((plan) => {
 });
 
 function calcular_iva(costo) {
-    const iva=costo*0.19;
+    const iva=parseInt(costo*0.19);
     return iva
   }
 
@@ -148,6 +148,7 @@ plans.forEach((plan) => {
     costoSpan.textContent = costo;
     costo_iva.textContent = iva;
     costo_total_span.textContent = costo_total;
+    plan_total_con_iva.textContent=costo_total;
     num_cotizacion_span.textContent = getRandomInt(100, 999);
 
     // Mostrar la plantilla
@@ -271,18 +272,137 @@ input.addEventListener("focus", function() {
 input.value = ""; // Borrar el contenido del input al hacer clic
 });
 
-/* PARTE DE DARLE CLICK AL BOTON ENVIAR */
+/*Creacion del array con los diferentes items extras*/
+// Array para almacenar elementos seleccionados
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtén todos los elementos de tipo checkbox en la página
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  
+  // Recorre todos los checkboxes y desmárcalos
+  checkboxes.forEach((checkbox) => {
+      checkbox.checked = false;
+  });
+});
+
+
+// Array para almacenar elementos seleccionados
+const selectedItems = [];
+let costoPlan = 100; // Precio del plan
+
+// Función para manejar el evento de clic en la casilla de verificación
+function handleCheckboxClick(checkbox, costo_total) {
+    if (checkbox.checked) {
+        // Si se selecciona, agregar al array y sumar 100 al costo total
+        selectedItems.push(checkbox.value);
+        // Calcular el costo total y actualizarlo
+        costo_final_cotizacion=calculateTotalCost_plus(costo_total);
+        console.log('SE HA AGREGADO UN NUEVO ITEM: +100 USD | ITEM: '+checkbox.value);
+    } else {
+        // Si se deselecciona, eliminar del array y restar 100 al costo total
+        const index = selectedItems.indexOf(checkbox.value);
+        if (index !== -1) {
+            selectedItems.splice(index, 1);
+            // Calcular el costo total y actualizarlo
+            costo_final_cotizacion=calculateTotalCost_menos(costo_total);
+            console.log('SE HA ELIMINADO UN ITEM: -100 USD | ITEM: '+checkbox.value);
+        }
+    }
+
+    // Mostrar los elementos seleccionados en la consola (puedes personalizar esto)
+    console.log("Elementos seleccionados:", selectedItems);
+
+    // Actualizar la tabla de cotización
+    updateTable(costo_final_cotizacion);
+}
+
+// Agregar eventos de clic para todas las casillas de verificación
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('click', () => {
+    const costo_total_act = document.getElementById("costo_total_span").textContent;
+    handleCheckboxClick(checkbox, costo_total_act);
+    });
+});
+
+// Función para actualizar la tabla de cotización con elementos seleccionados
+function updateTable(costo_final_cotizacion) {
+    // Obtener el elemento de la tabla de cotización
+    const cotizacionTable = document.getElementById("cotizacion_table");
+
+    // Eliminar solo las filas previamente agregadas para los elementos seleccionados
+    const selectedRows = document.querySelectorAll('.selected-row');
+    selectedRows.forEach((row) => {
+        cotizacionTable.removeChild(row);
+    });
+
+    // Agregar filas para los elementos seleccionados
+    selectedItems.forEach((item) => {
+        const newRow = document.createElement("tr");
+        newRow.classList.add('selected-row'); // Agregar clase para identificar las filas seleccionadas
+        newRow.style.backgroundColor = "#212529";
+        newRow.innerHTML = `
+            <td>1</td>
+            <td>${item}</td>
+            <td style="text-align: right;"><p><span>81</span> USD</p></td>
+            <td style="text-align: right;"><p><span>19</span> USD</p></td>
+            <td style="text-align: right;"><p><span>100</span> USD</p></td>
+        `;
+        cotizacionTable.appendChild(newRow);
+    });
+
+    // Mostrar el costo del plan en la tabla
+     const costo_total_span = document.getElementById("costo_total_span");
+    costo_total_span.textContent = costo_final_cotizacion;
+    console.log('VALOR FINAL COTIZACION:'+costo_final_cotizacion);
+    costo_total_span.textContent = costo_final_cotizacion.toFixed(2);
+}
+
+// Funcionnes para calcular el costo total
+function calculateTotalCost_plus(costo_total) {
+    let costo_final = parseFloat(costo_total)+ 100;
+    return costo_final
+}
+function calculateTotalCost_menos(costo_total) {
+  let costo_final = parseFloat(costo_total)- 100; 
+  return costo_final
+}
+
+
+/* CLICK AL BOTON ENVIAR */
 var boton_enviar_cotizacion = document.getElementById("boton_enviar_cotizacion");
 boton_enviar_cotizacion.addEventListener("click", () => {
     var tipo_plan = document.getElementById("plan").textContent;
 
 if (nombreInput.value!='' && emailInput.value!=''  && inputPais.value!=''){
+  // //Class
+      class Cliente{
+      constructor(nombre, email, pais){
+          this.nombre=nombre;
+          this.email=email;
+          this.pais=pais;
+          this.cotizado=false;
+      }
+      //metodos
+      enviar_cotizacion(){
+          this.cotizado=true;
+      }
+      aumentarPrecio(porcentaje){
+          this.precio=this.precio * porcentaje;
+      }
+      }
+      const client=new Cliente(nombreInput.value, emailInput.value, pais_span.textContent);
+      client.enviar_cotizacion();
+      console.log(client);
+
     Swal.fire({
         icon: 'success',
         title: '<h1 class="titulos_sweet_alert">Cotización a enviar</h1>',
         html: '<div class="image-container"><img src="../static/img/contento.png" alt="" style="width: 50px;"></div>' +
         '<div class="list-container_plan_alert">'+  
         '<h3 class="title_alert_plans">' + tipo_plan + '</h3>'+
+        '<p>Nombre: ' + client.nombre + '</p>'+
+        '<p>Email: ' + client.email + '</p>'+
+        '<p>Pais: ' + client.pais + '</p>'+
         '</div>',
         showCancelButton: false,
         confirmButtonText: 'OK',
@@ -308,3 +428,34 @@ else{
       });
 }
   });
+// //Class
+// class Computadora{
+//     constructor(tipo, marca, precio){
+//         this.tipo=tipo;
+//         this.marca=marca;
+//         this.precio=precio;
+//         this.vendido=false;
+//     }
+//     //metodos
+//     venderCompu(){
+//         this.vendido=true;
+//     }
+//     aumentarPrecio(porcentaje){
+//         this.precio=this.precio * porcentaje;
+//     }
+// }
+
+// const compu1=new Computadora('Desktop', 'Generica', 1000);
+// const compu2=new Computadora('Notebook', 'Apple', 2000);
+// const compu3=new Computadora('Desktop', 'HP', 1500);
+
+// console.log(compu2);
+
+// compu3.venderCompu();
+
+// console.log(compu3);
+
+// let aumento = parseInt(prompt('Ingresa el aumento que quieres aplicar'));
+// compu1.aumentarPrecio(1+(aumento/100));
+
+// console.log('Precio actualizado: U$'+compu1.precio);
